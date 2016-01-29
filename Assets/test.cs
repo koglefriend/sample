80d6eec8557ffc442ae598a174c10d6f13ff47f0
Sarun Rattanagulvaranon <sarunrattanagulvaranon@Sarun-Rattanagulvaranons-MacBook-Pro.local>
2016-01-30 01:01:51 +0700

    Debug

---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("A");
+		Debug.Log("B");
+		Debug.Log("C");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
