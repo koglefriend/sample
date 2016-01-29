04b7121324c2690feac26325240a0f7ecb3a5d14
Sarun Rattanagulvaranon <sarunrattanagulvaranon@Sarun-Rattanagulvaranons-MacBook-Pro.local>
2016-01-30 01:40:46 +0700

    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -6,11 +6,14 @@ public class test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		// Debug thing
 		Debug.Log("A");
-		Debug.Log("y");
+		Debug.Log("B");
 		Debug.Log("C");
 		Debug.Log("D");
-
+		Debug.Log("E");
+		Debug.Log("F");
+		Debug.Log("G");
 	
 	}
 	
